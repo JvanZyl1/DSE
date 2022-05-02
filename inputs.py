91de0e89d9bdb8fa52e8b1b41e2fbc0a9a911639
Oleksandr Krochak <O.O.Krochak@student.tudelft.nl>
2022-05-02 09:07:00 +0200

    added new files

---
+++ inputs.py
@@ -0,0 +1,10 @@
+'''Inputs script '''
+# Vehicle characteristics
+l = 6               # length of the vehicle in meters [m]
+D = 2               # diameter of the fuselage in meters [m]
+N_prop = 1          # number of propellers in meters squared [m2]
+D_prop = 5          # diameter of each propeller in meters [m]
+
+# Mission profile characteristics
+R = 20              # mission range in kilometers [km]
+R_div = 5           # additional diversion range in kilometers [km]
---
+++ main.py
@@ -0,0 +1,1 @@
+'''Conceptual Design of the Urban Air Vehicle'''
