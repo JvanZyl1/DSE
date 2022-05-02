d146754544c08ebeec47b5fb93ca24e782198075
Oleksandr Krochak <O.O.Krochak@student.tudelft.nl>
2022-05-02 11:24:49 +0200

    added the power estimation routine

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 '''Conceptual Design of the Urban Air Vehicle'''
+import numpy as np
+from inputs import *
+from PowerEstimation import PowerEstimatinonFun
+
+print(PowerEstimatinonFun(R_prop, N_prop, V_cr, omega_prop, rho, g, M_MTOW))
