2ab41ef0aa637749dc353e020b80de76e3ddb5a2
Tobias Evers <T.M.Evers-1@student.tudelft.nl>
2022-05-03 16:30:52 +0200

    Reference frames

--- Dynamics.py
+++ Dynamics.py
@@ -6,8 +6,11 @@ b = 0
 c = 0
 
 def T_x(a, mat):
-    Tx = np.matrix([[1,0,0][0,np.cos(a),np.sin(a)],[0,-1*np.sin(a),np.cos(a)]])
+    Tx = np.matrix([[1,0,0],[0,np.cos(a),np.sin(a)],[0,-1*np.sin(a),np.cos(a)]])
     mat_rot_x = mat*T_x
     return mat_rot_x
 
-print(Tx)
+def T_y(b, mat):
+    Ty = np.matrix([[np.cos(b),0,-1*np.sin(b)],[0,1,0],[np.sin(b),0,np.cos(b)]])
+    mat_rot_y = mat*t_y
+    return mat_rot_y
