aee62a0cb51cfcea22764d2f2747e0667947d986
jonatanvalk <j.valk-1@student.tudelft.nl>
2022-05-03 10:46:47 +0200

    Update MassEstimation.py

--- MassEstimation.py
+++ MassEstimation.py
@@ -1,5 +1,32 @@
-import numpy as np
 
+'''This scipt includes functions for weight estimation routines'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+from inputs import *
+n_TO = 1.5 # ratio of P_cruise / P_takeoff.
+
+def BatteryMassFun(R, R_div, V_cr, V_TO, h_TO, eta_E, P_cruise):
+    t_CR = (R + R_div) * 1000 / (V_cr / 3.6)     # Calculate time in cruise + diversion
+    t_TO = (h_TO / V_TO) * 2                     # Calculate the time spent in vertical flight
+    # Energy required for flight phases
+    E_CR = t_CR * P_cruise
+    E_TO = t_TO * P_cruise * n_TO
+    E_total = (E_TO + E_CR) / 3600               # total energy needed in [Wh]
+    M_bat = E_total / eta_E
+
+    print('The mass of the battery in [kg]: ', M_bat)
+
+    return M_bat
+
+def PowerPlantWeightEstFun(P_cruise):
+    W_pwp = 59.077 + P_cruise * 0.0004948372 # Weight of the powerplant in [kg]
+    return W_pwp
+    # using from  W = 130.243 + .369 HP_e
+
+def DriveTrainWeightEstFun(W_MTOW):
+    W_dt = -16.125 + W_MTOW * 0.045812829 # Weight of the drive train in [kg]
+    return W_dt
 
 def Est_rotor_mass(S_rot):
     '''Input rotor planform area'''
