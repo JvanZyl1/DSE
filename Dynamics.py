b17e3b68de8373406e41034dfd2279cba4012bae
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2022-05-03 16:28:41 +0200

    f

--- Dynamics.py
+++ Dynamics.py
@@ -5,6 +5,9 @@ a = 0
 b = 0
 c = 0
 
-Tx = np.matrix([[1,0,0][0,np.cos(a),np.sin(a)],[0,-1*np.sin(a),np.cos(a)]])
+def T_x(a, mat):
+    Tx = np.matrix([[1,0,0][0,np.cos(a),np.sin(a)],[0,-1*np.sin(a),np.cos(a)]])
+    mat_rot_x = mat*T_x
+    return mat_rot_x
 
 print(Tx)
