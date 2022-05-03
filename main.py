f09bb643866b16e1b6a60961dea0835ced19deb8
Tobias Evers <T.M.Evers-1@student.tudelft.nl>
2022-05-03 08:52:50 +0200

    Test for push

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 '''Conceptual Design of the Urban Air Vehicle'''
 import numpy as np
-from inputs import *
-from PowerEstimation import PowerEstimatinonFun
 
-print(PowerEstimatinonFun(R_prop, N_prop, V_cr, omega_prop, rho, g, M_MTOW))
