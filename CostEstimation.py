01901aca9a9d00b5277a1cb15fb0ce4c4dbe9ec0
Stijnk020 <stijnk020@gmail.com>
2022-05-09 10:23:43 +0200

    starr of cost esitmation

--- CostEstimation.py
+++ CostEstimation.py
@@ -1,2 +1,7 @@
 import numpy as np
 from inputs import *
+from MassEstimation import *
+from main import P_hov
+
+W_bat = BatteryMassFun(R, R_div, V_cr, V_TO, h_TO, eta_E, P_hov, P_cruise, nu_discharge)
+W_s = StructureMassFun(n_ult, D, l, MTOW)
