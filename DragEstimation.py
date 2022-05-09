7ce689cbfce2728e573f8d4c1a13459da5ad811e
jonatanvalk <j.valk-1@student.tudelft.nl>
2022-05-09 11:03:11 +0200

    Added windforces

--- DragEstimation.py
+++ DragEstimation.py
@@ -17,3 +17,18 @@ def RC_AoA(V_cr, D_q_tot, rho, MTOW, g):
     #Equilibrium AoA
     alpha = np.atan2(D,MTOW * g)
     return alpha
+
+def Windforces(rho,Vx, Vy, Vz, V_ind, Vw_x, Vw_y, Vw_z, D_q_tot_x):
+    '''Velocities and wind velocities in bodyframe.'''
+    Vx_rel = Vx - Vw_x
+    Vy_rel = Vy - Vw_y
+    Vz_rel = Vz - Vw_z
+    V_infty = np.sqrt(Vx_rel**2 +Vy_rel**2 +(Vz_rel + V_ind)**2)
+
+    D_q_tot_y = 2.2 * D_q_tot_x # [m^2] Assumption
+    D_q_tot_z = 1.5 * D_q_tot_x # [m^2] Assumption
+    #The above assumptions come from http://www.israelbarrientos.org/personal/simulador_files/helimodel_gavrilet.pdf
+    Fw_x = -0.5 * rho * Vx_rel * V_infty * D_q_tot_x
+    Fw_y = -0.5 * rho * Vy_rel * V_infty * D_q_tot_y
+    Fw_z = -0.5 *rho *(Vz_rel + V_ind) * V_infty * D_q_tot_z
+    return Fw_x, Fw_y, Fw_z
