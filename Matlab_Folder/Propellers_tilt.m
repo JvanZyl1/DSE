fffacb97d17147bd207b5c5b184c1cc663bff5b2
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2022-05-11 12:21:28 +0200

    'g'

--- Matlab_Folder/Propellers_tilt.m
+++ Matlab_Folder/Propellers_tilt.m
@@ -4,10 +4,11 @@ clc
 
 h = 0.1; % Height of prop
 h_mass_prop = 0.5; %Where the prop_cg is placed in relation to prop height
-theta_0 = pi/2; %Initial angle
-theta_final = 3/2 * pi; %Final angle
+theta_0 = [0; 0; 0]; %Initial angle
+theta_final = [pi; 0; 0]; %Final angle
+w_0 = [0;0;0];
 T_prop = 500;
-I_mat = [10 0 0;
+I_mat = [2 10 0 ;
     0 10 0;
     0 0 10];
 I_inv = inv(I_mat);
