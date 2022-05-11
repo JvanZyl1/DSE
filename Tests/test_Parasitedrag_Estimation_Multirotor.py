0240952377a08fc72a79acbbf8acbce24a35ec13
Omkar <O.K.Vaidya@student.tudelft.nl>
2022-05-11 11:28:39 +0200

    finished version

--- Tests/test_Parasitedrag_Estimation_Multirotor.py
+++ Tests/test_Parasitedrag_Estimation_Multirotor.py
@@ -3,8 +3,8 @@ import unittest
 import numpy as np
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# import ../Parasitedrag_Estimation_Multirotor.py
-#Import the file directly as shown
+# Import ../Parasitedrag_Estimation_Multirotor.py
+# Import the file directly as shown
 # to import from other folder, do parent folder.(other_folder).file_name
 import Parasitedrag_Estimation_Multirotor as pm
 
@@ -15,12 +15,17 @@ class MyTestCase(unittest.TestCase):
     In every function, you can call the function to test, and assert it equal/almostequal/greaterthan...
     When running this file, it returns if the functions performed as expected. You will see what tests passed or failed.
     '''
-    def test_parasite_drag(self):
+    def test_parasite_drag1(self):
 
         actual_CD0 = 0.644
         test_CD0 = pm.parasite_drag()[0]
         self.assertAlmostEqual(actual_CD0, test_CD0, 2)  # add assertion here
-    
+
+    def test_parasite_drag2(self):
+        actual_D_q_tot_x = 0.5057
+        test_D_q_tot_x = pm.parasite_drag()[1]
+        self.assertAlmostEqual(actual_D_q_tot_x, test_D_q_tot_x, 2)
+
 
 
 
