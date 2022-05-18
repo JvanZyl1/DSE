de73992e671dc47e02f98655e77612abacd493bb
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2022-05-18 11:38:22 +0200

    'g'

--- Matlab_Folder/six.m
+++ Matlab_Folder/six.m
@@ -2,9 +2,22 @@ clear all
 close all
 clc
 
+I_xx = 1.2;
+I_yy = 1.0;
+I_zz = 0.5;
 
-Inertia = [1.2 0 0;
-    0 1.0 0;
-    0 0 0.5];
+Inertia = [I_xx 0 0;
+    0 I_yy 0;
+    0 0 I_zz];
 
-Inertia_inverese = inv(Inertia)
+Inertia_inverse = inv(Inertia);
+
+theta_0 = [0;0;0];
+w_0 = [0;0;0];
+theta_z_f = pi;
+q_0 = [1; 0; 0; 0];
+
+T_max = 5000; %Nm
+dwx = T_max/I_xx;
+dwy = T_max/I_yy;
+dwz = T_max/I_zz;
