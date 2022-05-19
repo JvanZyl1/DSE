e532b978f2955f05c0970aed1ae66064c6501adb
Stijnk020 <stijnk020@gmail.com>
2022-05-19 15:56:10 +0200

    'hey'

--- PowerEstimation.py
+++ PowerEstimation.py
@@ -77,12 +77,12 @@ from MassEstimation import BatteryMassFun
 
 def PowerReq(MTOW,N_prop,R_prop,V_cr):
     """Function designed for multirotors (EHang's)"""
-    T = (MTOW * g) * 1.1       #10 percent safety factor
-    tilt_cruise = RC_AoAandThrust(V_cr, parasite_drag()[1], rho, MTOW, g)[0]*180/np.pi       #angle of tilt during cruise in degree
-    CD0, D_q_tot_x = parasite_drag()
-    tilt_cruise = RC_AoAandThrust(V_cr, D_q_tot_x, rho, MTOW, g)[0]*180/np.pi       #angle of tilt during cruise in degree
-    disk_area = R_prop**2 * np.pi * N_prop
-    kappa = 1.2       #correction factor for extra power losses, value taken from literature
+    T = (MTOW * g) * 1.1 ;      #10 percent safety factor
+    tilt_cruise = RC_AoAandThrust(V_cr, parasite_drag()[1], rho, MTOW, g)[0]*180/np.pi  ;       #angle of tilt during cruise in degree
+    CD0, D_q_tot_x = parasite_drag()   ;
+    tilt_cruise = RC_AoAandThrust(V_cr, D_q_tot_x, rho, MTOW, g)[0]*180/np.pi  ;     #angle of tilt during cruise in degree
+    disk_area = R_prop**2 * np.pi * N_prop ;
+    kappa = 1.2   ;    #correction factor for extra power losses, value taken from literature
     V_perp = (V_cr * np.sin(tilt_cruise * (np.pi/180)))      #perpendicular to rotor plane free stream velocity in [m/s]
     v_i = np.sqrt((T/disk_area) * (1/(2 * rho)))           #induced velocity during hover
     P = T*V_perp + kappa * T * (-V_perp/2 + np.sqrt(V_perp**2 / 4 + T/(2 * rho * disk_area)))
