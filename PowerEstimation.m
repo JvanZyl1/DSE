03423e8e67ee25b01ff50001f5d0feb4cded3ca5
Stijnk020 <stijnk020@gmail.com>
2022-05-19 16:14:22 +0200

    'I_wanna_end_it_all'

--- PowerEstimation.m
+++ PowerEstimation.m
@@ -3,19 +3,19 @@ close all
 clc
 
 inputs;
-eta_power_transfer
+RC_AoAandThrust(V_cr, D_q_tot_x, rho, MTOW, g);
 
 
 function [P_cruise,P_TOL] = PowerReq(MTOW,N_prop,R_prop,V_cr)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 T = (MTOW * g) * 1.1;
-tilt_cruise = RC_AoAandThrust(V_cr, parasite_drag()[1], rho, MTOW, g)[0]*180/np.pi  ;       %angle of tilt during cruise in degree
+tilt_cruise = RC_AoAandThrust(V_cr, parasite_drag()[1], rho, MTOW, g)(1)*180/pi  ;       %angle of tilt during cruise in degree
 CD0, D_q_tot_x = parasite_drag()   ;
-tilt_cruise = RC_AoAandThrust(V_cr, D_q_tot_x, rho, MTOW, g)[0]*180/np.pi  ;     %angle of tilt during cruise in degree
-disk_area = R_prop**2 * np.pi * N_prop ;
+tilt_cruise = RC_AoAandThrust(V_cr, D_q_tot_x, rho, MTOW, g)[0]*180/pi  ;     %angle of tilt during cruise in degree
+disk_area = R_prop^2 * pi * N_prop ;
 kappa = 1.2   ;    %correction factor for extra power losses, value taken from literature
-V_perp = (V_cr * np.sin(tilt_cruise * (np.pi/180))) ;     %perpendicular to rotor plane free stream velocity in [m/s]
+V_perp = (V_cr * np.sin(tilt_cruise * (pi/180))) ;     %perpendicular to rotor plane free stream velocity in [m/s]
 v_i = sqrt((T/disk_area) * (1/(2 * rho)))     ;      %nduced velocity during hover
 P = T*V_perp + kappa * T * (-V_perp/2 + sqrt(V_perp^2 / 4 + T/(2 * rho * disk_area))) ;
 P_cruise = P / eta_final   ;
---
+++ RC_AoAandThrust.m
@@ -0,0 +1,16 @@
+clear all
+close all
+clc
+
+inputs;
+
+function [alpha, Treq] = RC_AoAandThrust(V_cr, D_q_tot_x, rho, MTOW, g)
+'Rotor craft angle of attack estimator'
+%Nominal drag force on fuselage during cruise
+D = 0.5 * rho * V_cr^2* D_q_tot_x  ;
+%Equilibrium AoA ;
+alpha = atan2(D,MTOW * g)  ;
+Treq = np.sqrt((MTOW*g)^2 + D^2) ;
+
+
+end
