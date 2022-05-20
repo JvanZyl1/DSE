158fc2233bb39da00d77b81ddff083538c4fbd2f
Stijnk020 <stijnk020@gmail.com>
2022-05-20 09:23:25 +0200

    'ididitmyself'

--- StijnsTests.m
+++ StijnsTests.m
@@ -2,14 +2,14 @@ clear all
 close all
 clc
 
-global l_pyl R_pyl N_prop D
+%global l_pyl R_pyl N_prop D
 
 inputs;
 
 
 parasite_drag();
-RC_AoAandThrust(V_cr, D_q_tot_x, rho, MTOW, g);
+RC_AoAandThrust(V_cr, rho, MTOW, g);
 
-PowerReq(MTOW,N_prop,R_prop,V_cr)
+[P_cr,P_takeoff] = PowerReq(MTOW,N_prop,R_prop,V_cr)
 
 
