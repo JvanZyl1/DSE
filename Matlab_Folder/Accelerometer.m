d3dec8af94175a6210c895997b669d1311693be4
tobiasevers <93926687+tobiasevers@users.noreply.github.com>
2022-05-23 11:04:12 +0200

    Update Accelerometer.m

--- Matlab_Folder/Accelerometer.m
+++ Matlab_Folder/Accelerometer.m
@@ -3,35 +3,43 @@
 params = accelparams 
 
 % Parameters for simulated signal
-N = 1000;       % Number of samples
+N = 1;       % Number of samples
 Fs = 100;       % Sampling rate
 Fc = 0.25;      % Sinusoidal frequency
 
 
 % Create arrays
 t = (0:(1/Fs):((N-1)/Fs)).';
-acc = zeros(N, 3);          %Input accelerations
+acc = [5,-5,-5];          %Input accelerations
 orient = quaternion.ones(N, 1);
 angvel = zeros(N, 3);
+%acc(:,1) = 8*sin(2*pi*Fc*t)
+%acc(:,2) = sin(2*pi*Fc*t)
+%acc(:,3) = 4
 
 % Generate acceleration data
 imu = imuSensor('SampleRate', Fs, 'Accelerometer', params);
-imu.Accelerometer.MeasurementRange = 10;   % Maximum acceleration accelerometer can measure
+imu.Accelerometer.MeasurementRange = 15;   % Maximum acceleration accelerometer can measure
 imu.Accelerometer.Resolution = 1/Fs;           % Step size of digital measurements
 
 % Biases
-imu.Accelerometer.BiasInstability = 7.5 / 1000;
-imu.Accelerometer.NoiseDensity = 150 / 1000 / 1000;
-imu.Accelerometer.TemperatureBias = 50 / 1000 / 1000;
+imu.Accelerometer.BiasInstability = 7.5 / 1000 * 9.81;
+imu.Accelerometer.NoiseDensity = 150 / 1000 / 1000 * 9.81;
+imu.Accelerometer.TemperatureBias = 50 / 1000 / 1000 * 9.81;
 
 
-imu = imuSensor('SampleRate', Fs, 'Accelerometer', params);
+accelData = imu(acc, angvel, orient)
 
-accelData = imu(acc, angvel, orient);
+accelData(:,3) = accelData(:,3) - 9.81
+
+accelData(:,1) = accelData(:,1) * -1
+accelData(:,2) = accelData(:,2) * -1
+accelData(:,3) = accelData(:,3) * -1
+
 
 % Plot simulated signal
 figure
-plot(t,accelData)
+plot(t, acc, t,accelData)
 xlabel('Time (s)')
 ylabel('Acceleratio (m/s^2)')
 title('Ideal Acceleration Data')
