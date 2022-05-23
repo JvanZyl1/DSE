9ff04f9afe09723703615f7beb7e75aa0ea3407a
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2022-05-23 11:18:55 +0200

    'g'

--- Matlab_Folder/V&V/LinearDynamics_VV.m
+++ Matlab_Folder/V&V/LinearDynamics_VV.m
@@ -13,7 +13,7 @@ r_trajectory = [2;3;4];
 a_accelerometer_error = [1;2;3];
 
 
-i = 9;
+i = 13;
 if i == 1
     %Unit Test 1 : mass equal to 0
     m = 0;
@@ -59,4 +59,46 @@ elseif i == 9
     clear r_trajectory
     error("Unit Test 9 passed.")
     sim('LinearDynamics_V', 10)
+elseif i == 10
+    %Unit Test 10 : F_aero is a row vector
+    F_aero = transpose(F_aero);
+    error("Unit Test 10 passed.")
+    sim('LinearDynamics_V',10)
+elseif i == 11
+    %Unit Test 11 : a_accelerometer_error is a row vector
+    a_accelerometer_error = transpose(a_accelerometer_error);
+    error("Unit Test 11 passed.")
+    sim('LinearDynamics_V',10)
+elseif i == 12
+    %Unit Test 12 : r_trajectory is a row vector
+    r_trajectory = transpose(r_trajectory);
+    error("Unit Test 12 passed.")
+    sim('LinearDynamics_V',10)
+elseif i == 13
+    %System Test 13 : increasing force
+    sim('LinearDynamics_V', 10)
+    subplot(2,2,1)
+    plot(ans.F1);
+    set(gca,'FontSize',16)
+    xlabel('t [s]', 'fontsize', 16);
+    ylabel('F [N]', 'fontsize', 16);
+    title('Time vs Force', 'fontsize', 16);
+    subplot(2,2,2)
+    plot(ans.g_force1);
+    set(gca,'FontSize',16)
+    xlabel('t [s]', 'fontsize', 16);
+    ylabel('a [g]', 'fontsize', 16);
+    title('Time vs Acceleration', 'fontsize', 16);
+    subplot(2,2,3)
+    plot(ans.v1);
+    set(gca,'FontSize',16)
+    xlabel('t [s]', 'fontsize', 16);
+    ylabel('v [ m/s]', 'fontsize', 16);
+    title('Time vs Velcity', 'fontsize', 16);
+    subplot(2,2,4)
+    plot(ans.r1);
+    set(gca,'FontSize',16)
+    xlabel('t [s]', 'fontsize', 16);
+    ylabel('r [m]', 'fontsize', 16);
+    title('Time vs Position', 'fontsize', 16);
 end
