6a2ce64df676fe83a472c096d0520ec5e7125259
Stijnk020 <stijnk020@gmail.com>
2022-05-24 17:26:00 +0200

    'endsesh'

--- RC_AoAandThrust.m
+++ RC_AoAandThrust.m
@@ -1,5 +1,7 @@
 function [alpha, Treq] = RC_AoAandThrust(V_cr_man, MTOW)
 inputs;
+%MTOW = 700;
+%V_cr_man = V_cr;
 parasite_drag();
 [CD0, D_q_tot_x] = parasite_drag();
 %global V_cr D_q_tot_x rho MTOW g 
@@ -9,6 +11,6 @@ D = 0.5 * rho * V_cr_man^2* D_q_tot_x  ;
 %Equilibrium AoA ;
 alpha = atan2(D,MTOW * g)  ;
 Treq = sqrt((MTOW*g)^2 + D^2) ;
-
+alpha
 
 end
