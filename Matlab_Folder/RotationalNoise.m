52530198f4ee8f5f3ee8037f87baa02e47c7bf0d
jonatanvalk <j.valk-1@student.tudelft.nl>
2022-06-02 16:43:06 +0200

    Update noise calcs

--- Matlab_Folder/RotationalNoise.m
+++ Matlab_Folder/RotationalNoise.m
@@ -1,4 +1,4 @@
-function SPL = RotationalNoise(theta,S,m,B,omega,a,T,Q,R_e,rho,N)
+function SPL = RotationalNoise(theta,S,m,B,omega,a,T,Q,R_e,rho,N,c,t_max)
     %https://tud365.sharepoint.com/sites/UrbanTurbulence/Gedeelde%20documenten/Forms/AllItems.aspx?FolderCTID=0x0120000A9B2D4B22DBA94F9BFBB3D4E117EB61&id=%2Fsites%2FUrbanTurbulence%2FGedeelde%20documenten%2FGeneral%2FLiterature%2FApproach%20for%20aeroacoustics%2FNoiseModel%2Epdf&parent=%2Fsites%2FUrbanTurbulence%2FGedeelde%20documenten%2FGeneral%2FLiterature%2FApproach%20for%20aeroacoustics
     %theta is azimuthal angle ( angle of observer with respect to the
     %vehicle -Z axis) [rad]
@@ -13,21 +13,22 @@ function SPL = RotationalNoise(theta,S,m,B,omega,a,T,Q,R_e,rho,N)
     % rho is the air density [kg m^-3]
     % N is the number of rotors [N]
     % Rotors are assumed to be identical. Ask me if you want it different.
-
-    pref = 2e5; %Reference pressure [Pa]
+    
+    pref = 2e-5; %Reference pressure [Pa]
 
     function p_m_L = LoadingNoise(theta,S,m,B,omega,a,T,Q,R_e)
         % Estimation of loading noise root mean square sound pressure
-        J=besselj(m*B,(m*B*omega/a)*R_e*sin(theta)); %First order bessel function
-        p_m_L= m*B*omega/(2*sqrt(2)*pi*a*S)*(T*cos(theta)-Q*a/(omega*R_e^2))*J;
+        J=besselj(m*B,(m*B*omega/a)*R_e.*sin(theta)); %Bessel function of first kind
+        p_m_L= m*B*omega/(2*sqrt(2)*pi*a*S)*(T.*cos(theta)-Q.*a./(omega*R_e^2)).*J;
     end
     function p_m_T = ThicknessNoise(theta,S,rho,m,B,omega,c,t_max,R_e,a)
         % Estimation of thickness noise root mean square sound pressure
-        J=besselj(m*B,(m*B*omega/a)*R_e*sin(theta));%First order bessel function
-        p_m_T = -rho*(m*B*omega)^2*B/(3*sqrt(2)*pi*S)*c*t_max*R_e*J;
+        J=besselj(m*B,(m*B*omega/a)*R_e.*sin(theta)); %Bessel function of first kind
+        p_m_T = -rho*(m*B*omega)^2*B/(3*sqrt(2)*pi*S)*c*t_max*R_e.*J;
     end 
     pmL = LoadingNoise(theta,S,m,B,omega,a,T,Q,R_e);
-    pmT = ThicknessNoise(theta,S,rho,m,B,omega,c,t_max,R_e);
+    pmT = ThicknessNoise(theta,S,rho,m,B,omega,c,t_max,R_e,a);
     %Sound pressure level in dB from sound pressure
-    SPL = 10*log10(N*(pmL^2+pmT^2)/pref^2);
+    display(N*(pmL.^2+pmT.^2)/pref^2)
+    SPL = 10*log10(N*(pmL.^2+pmT.^2)/pref^2);
 end
