3ef4e0a0ffe272c6c2738e3e28519f2ce9c5836a
odekoning <o.m.dekoning@student.tudelft.nl>
2022-06-02 08:33:27 +0200

    'rpmalmostdone'

--- Optimization/MassEstimationScripts/BatteryMassFun.m
+++ Optimization/MassEstimationScripts/BatteryMassFun.m
@@ -2,9 +2,11 @@ function [W_bat, E_total, V_bat] = BatteryMassFun(V_cr, P_cruise, P_TOL, P_cont)
     inputs;
     % eta_E = eta_E * 1.08^(yop-2022);   % Fulvio recommended NOT to use this
     t_cr = R / V_cr  ;   % Calculate time in cruise + diversion
-    t_TO = (h_TO / V_TO) * 2   ;                  % Calculate the time spent in vertical flight
-    t_cont = t_TO  ;
-    % Energy required for flight phases
+    t_TO = (h_TO / V_TO) * 2   ;   % Calculate the time spent in vertical flight
+    t_cont = t_TO  ;  % Control propulsion time is overestimated
+
+    % Energy required for flight phases, redundancy factor is taken out of
+    % the battery sizing
     E_cr = t_cr * P_cruise * redundancy_factor ;
     E_TO = t_TO * P_TOL * redundancy_factor  ;
     E_cont = t_cont * P_cont  ;
--- Optimization/angular_acc.m
+++ Optimization/angular_acc.m
@@ -5,5 +5,5 @@ Torque = W_m * 10 - 12;  % From literature
 MOI = (1/3) * B * W_b * R^2;
 
 Ang_acc = Torque / MOI;
-
+%fprintf('Torque = %f [Nm]', Torque)
 end
