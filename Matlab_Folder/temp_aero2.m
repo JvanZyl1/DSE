25b56b5331a6c968eb61878aec1a6056aea8816a
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2022-06-03 14:49:19 +0200

    'g'

--- Matlab_Folder/temp_aero2.m
+++ Matlab_Folder/temp_aero2.m
@@ -9,6 +9,8 @@ P = [0, 0, -1.32;
     30, 90, 0.32;
     -15, 90, 0.32];
 x = P(:,1) ; y = P(:,2) ; z = P(:,3) ;
+x = [abs(x); -abs(x); abs(x); -abs(x)];
+y = [abs(y);
 %x = [0, 0, 0, 0, 0, 15, -15, 0, 0, 0];
 %y =[0, 90, 45, 135, 180, 0, 0, 90, 90, 90];
 %z = [-1.32, 0.29, -1.15, 1.29, 1.29, -1.33, -1.48, 0.3, 0.32, 0.32 ];
@@ -16,6 +18,11 @@ ti = 0:1:180;
 [XI, YI] = meshgrid(ti,ti);
 model = scatteredInterpolant(x, y, z, 'linear', 'linear');
 ZI = model(XI, YI);
-surf(XI,YI,ZI), hold
-plot3(x,y,z,'ro'), hold off
+fig1 = figure(1)
+surf(XI,YI,ZI); shading interp; hold on
+plot3(x,y,z,'ro'); hold on
+plot3(-x, y, z, 'ro') 
+plot3(x, -y, z, 'ro')
+plot3(-x, -y, z, 'ro')
 colorbar
+hold off
