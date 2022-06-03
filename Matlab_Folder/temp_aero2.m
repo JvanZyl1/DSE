d6843fe54cc4b8a9140ee2f5a462261372ff9499
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2022-06-03 15:04:18 +0200

    'g'

--- Matlab_Folder/temp_aero2.m
+++ Matlab_Folder/temp_aero2.m
@@ -9,20 +9,25 @@ P = [0, 0, -1.32;
     30, 90, 0.32;
     -15, 90, 0.32];
 x = P(:,1) ; y = P(:,2) ; z = P(:,3) ;
+x1 = x; y1 = y; z1 = z;
 x = [abs(x); -abs(x); abs(x); -abs(x)];
-y = [abs(y);
+y = [abs(y); abs(y); -abs(y); -abs(y)];
+z = [z; z; z; z];
 %x = [0, 0, 0, 0, 0, 15, -15, 0, 0, 0];
 %y =[0, 90, 45, 135, 180, 0, 0, 90, 90, 90];
 %z = [-1.32, 0.29, -1.15, 1.29, 1.29, -1.33, -1.48, 0.3, 0.32, 0.32 ];
-ti = 0:1:180;
+ti = -180:1:180;
 [XI, YI] = meshgrid(ti,ti);
 model = scatteredInterpolant(x, y, z, 'linear', 'linear');
 ZI = model(XI, YI);
 fig1 = figure(1)
 surf(XI,YI,ZI); shading interp; hold on
-plot3(x,y,z,'ro'); hold on
-plot3(-x, y, z, 'ro') 
-plot3(x, -y, z, 'ro')
-plot3(-x, -y, z, 'ro')
+plot3(x1,y1,z1,'ro'); hold on
+% plot3(-x, y, z, 'ro') 
+% plot3(x, -y, z, 'ro')
+% plot3(-x, -y, z, 'ro')
 colorbar
 hold off
+zlabel('$C_X$ [$~$]', 'Interpreter','Latex');
+xlabel('$\beta$ [$\deg$]', 'Interpreter','Latex');
+ylabel('$\alpha$ [$\deg$]', 'Interpreter','Latex');
